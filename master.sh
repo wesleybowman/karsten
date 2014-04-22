6f383ff902f59f44c72dbacbe805c6ed55d2c8e3
wesleybowman <wesley.bowman23@gmail.com>
2014-04-22 15:54:00 -0300

    added qsub

--- master.sh
+++ master.sh
@@ -5,14 +5,14 @@ year="$2"
 python makeFirstRun.py $gridName $year
 
 export gridName
-./RUN_PAR2.sh
+qsub RUN_PAR2.sh
 wait ${!}
 python moveFile $gridName $year 01
 
 for month in {02..12}
 do
     python makeRun.py $gridName $year $month
-    ./RUN_PAR2.sh
+    qsub RUN_PAR2.sh
     wait ${!}
     python moveFile $gridName $year $month
 done
