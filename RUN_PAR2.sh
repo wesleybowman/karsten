18e84f9422a904c9dda63e6fd897333782d6ae52
wesleybowman <wesley.bowman23@gmail.com>
2014-04-22 14:39:43 -0300

    made Bash script and added RUN_PAR2

--- RUN_PAR2.sh
+++ RUN_PAR2.sh
@@ -4,8 +4,7 @@
 #$ -R y
 #$ -j y
 #$ -l h_rt=47:59:00
-#$ -pe ompi 128
+#$ -pe ompi 64
 gridname=$gridName
-echo "$gridname"
 time mpirun ./fvcom2d_fast  --casename=$gridname>run_output
 echo "Application ends at `date`"
