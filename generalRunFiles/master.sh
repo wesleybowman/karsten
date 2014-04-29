7c19f4ea30870083448632fbd74eaf6370a40366
wesleybowman <wesley.bowman23@gmail.com>
2014-04-29 17:18:47 -0300

    tidied up

--- generalRunFiles/master.sh
+++ generalRunFiles/master.sh
@@ -84,22 +84,3 @@ fi
 
 python makeRun.py
 python runpar.py
-#RUN_PAR2.sh>>RUNTEST.sh
-
-#gridName="$1"
-#year="$2"
-#
-#python makeFirstRun.py $gridName $year
-#
-#export gridName
-#python myQSUB.py
-#python moveFile.py $gridName $year 01
-#wait ${!}
-#
-#for month in {02..12}
-#do
-#    python makeRun.py $gridName $year $month
-#    python myQSUB.py
-#    python moveFile.py $gridName $year $month
-#    wait ${!}
-#done
