3f0080397c5f9206abc59e279212db05161563b1
wesleybowman <wesley.bowman23@gmail.com>
2014-06-30 08:33:15 -0300

    fixing calculations

--- project/vertical_spatial.py
+++ project/vertical_spatial.py
@@ -65,13 +65,13 @@ if t_slice.shape[0] != 1:
 #vel = np.sqrt(nc['u'][argtime,:,el]**2+nc['v'][argtime,:,el]**2+nc['ww'][argtime,:,el]**2)
 #vel = np.sqrt(data.u[argtime,:,el]**2+data.u[argtime,:,el]**2+data.ww[argtime,:,el]**2)
 print 'Calculating'
-print len(el)
+print len(el[0])
 #vel = np.sqrt(data.u[:, :, el[0]]**2 + data.v[:, :, el[0]]**2 + data.ww[:, :, el[0]]**2)
-u = data.u[:, :, el]
-v = data.v[:, :, el]
-ww = data.ww[:, :, el]
-#vel = ne.evaluate('sqrt(u**2 + v**2 + ww**2)')
-vel = np.sqrt(u**2 + v**2 + ww**2)
+u = data.u[:, :, el[0]]
+v = data.v[:, :, el[0]]
+ww = data.ww[:, :, el[0]]
+vel = ne.evaluate('sqrt(u**2 + v**2 + ww**2)')
+#vel = np.sqrt(u**2 + v**2 + ww**2)
 
 mean_vel = np.mean(vel, axis=0)
 
