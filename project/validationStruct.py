cab514616d4ce1f43acca1973264b88fcd9ffc80
wesleybowman <wesley.bowman23@gmail.com>
2014-07-15 13:25:47 -0300

    testing

--- project/validationStruct.py
+++ project/validationStruct.py
@@ -176,7 +176,7 @@ def main(debug=False):
                             cnstit='auto', rmin=0.95, notrend=True,
                             method='ols', nodiagn=True, linci=True, coef_int=True)
 
-            adcpElevCoef = ut_solv(adcpData.mtime, adcpData.surf,
+            adcpElevCoef = ut_solv(adcpData.mtime, adcpData.surf.flatten(),
                             [], adcpData.lat[0],
                             cnstit='auto', rmin=0.95, notrend=True,
                             method='ols', nodiagn=True, linci=True, coef_int=True)
