ee2bf8d830119ae2bbe67790499d4b2d580905f4
wesleybowman <wesley.bowman23@gmail.com>
2014-07-23 11:08:53 -0300

    testing ind

--- project/validationStruct.py
+++ project/validationStruct.py
@@ -24,9 +24,12 @@ def closest_point(points, lon, lat):
 
     point_list = np.array([lon,lat]).T
 
+    print point_list
+    print points
     closest_dist = ((point_list[:, 0] - points[:, 0, None])**2 +
                     (point_list[:, 1] - points[:, 1, None])**2)
 
+    print closest_dist
     closest_point_indexes = np.argmin(closest_dist, axis=1)
 
     return closest_point_indexes
@@ -93,6 +96,7 @@ def main(fvFiles, adcpFiles, tideFiles, isStation=True, ax=[], debug=False):
                 ind = closest_point(lonlat, fvData.lon, fvData.lat)
             else:
                 fvData = FVCOM(fvFile, ax)
+                print fvData.matches
                 print ax
                 lonlat = np.array([[adcpData.lon[0][0],
                                    adcpData.lat[0][0]]])
